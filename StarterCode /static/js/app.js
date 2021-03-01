b71d55791f22510237f627ad8d7631e9267848b0
Marcellisv <marcellisv@gmail.com>
2021-03-01 17:37:56 -0500

    app.js

--- StarterCode /static/js/app.js
+++ StarterCode /static/js/app.js
@@ -1,139 +1,116 @@
-var data;
+// fuction init
+function init()
+{
+    var dropdownMenu = d3.select("selDataset");
+    d3.json("samples.json").then((sample_data)=>
+        {
+          console.log(sample_data.names);
 
-//init function to fill in the select option
-
-function init() {
-  d3.json("data/samples.json").then(incomingData => {
-    data = incomingData;
-    var selectValues = incomingData.names;
-    var selectOption = d3.select("#selDataset");
-
-    selectValues.forEach(value => {
-      selectOption
-        .append("option")
-        .text(value)
-        .attr("value", function() {
-          return value;
+            var sample_id = sample_data.names;
+            sample_id.forEach((sample)=>
+            {
+                //apppend sample_id into dropdown
+                dropdownMenu.append("option").text(sample).property("value",sample);
+            });
+            var first_id = sample_id[0]
+            build(first_id)
+            buildplots(first_id)
         });
+}
+init();
+
+//Build Demographic table
+
+function build(sample)
+{
+    d3.json("samples.json").then((sample_data) =>
+    {
+      console.log(sample_data);
+
+        var sampleMetadata = sample_data.metadata;
+
+
+        var results = sampleMetadata.filter(s=>s.id == sample);
+        console.log(results);
+
+        let panel = d3.select("#sample-metadata");
+        panel.html("");
+        Object.entries(results[0]).forEach(([key,value]) =>
+        {
+        panel.append("h6").text(`${key}:${value}`);
+      });
     });
-    d3.selectAll("#selDataset").on("change", plotFunctions);
-    d3.select('#selDataset').property('value', 940);
-  
-    plotFunctions();
+};
+
+//Build Plots
+function buildplots(sample)
+{
+  d3.json('samples.json').then((sample_data) =>
+  {
+    var samples = sample_data.samples;
+    var results = samples.filter(s=>s.id == sample);
+    console.log("results");
+    console.log(results);
+        
+    var graphData = results[0];
+
+    console.log(graphData)
+
+
+//Grab top 10 OTUs for the plots
+    var sample_value = graphData.sample_value;
+    var otu_id = graphData.otu_id;
+    var otu_labels = graphData.otu_labels;
+
+// Build Bar chart
+    var bar_trace =
+    {
+        x: sample_value.slice(0,10),
+        y: otu_id.slice(0,10).map(value=>`OTU ID ${value}`).reverse(),
+        type: "bar",
+        text: otu_labels.slice(0,10).reverse(),
+        orientation: "h"
+    };
+
+    bar_data = [bar_trace];
+    // Create Bar Layout
+
+    var bar_layout = 
+    {
+      xaxis: { title: "Sample ID"},
+    };
+
+Ploty.newPlot("bar", bar_Data, bar_layout);
+
+//Create Bubble Plot 
+
+var bubble_trace = 
+{
+  x: otu_id,
+  y:sample_value,
+  mode: "markers",
+  marker:
+  {
+   color: otu_id, 
+   size: sample_value
+
+  },
+  text: otu_labels,
+};
+
+var bubble_data = [bubble_trace];
+var bubble_layout = 
+{
+  xaxis: {title: "otu_id"},
+  yaxis: {title: "sample_value"}
+};
+Plotly.newPlot("bubble", bubble_Data, bubble_layout);
+
   });
 };
-
-//functions to clean and create bacteria/ouid lists
-function fixBacteria(name) {
-    var listBacteria = [];
   
-    for (var i = 0; i < name.length; i++) {
-      var stringName = name[i].toString();
-      var splitName = stringName.split(";");
-      if (splitName.length > 1) {
-        listBacteria.push(splitName[splitName.length - 1]);
-      } else {
-        listBacteria.push(splitName[0]);
-      }
-    }
-    return listBacteria;
-  }
-  
-  function fixOuid(name) {
-    var listOuid = [];
-    for (var i = 0; i < name.length; i++) {
-      listOuid.push(`OTU ${name[i]}`);
-    }
-    return listOuid;
-  }
-
-
-function plotFunctions() {
-  var valueSelect = d3.select("#selDataset").node().value;
-  console.log(valueSelect);
-  demographicInfo(valueSelect);
-  barChart(valueSelect);
-  bubbleChart(valueSelect);
-  gaugeChart(valueSelect);
+function optionChanged(sample)
+{
+    build(sample);
+    buildplots(sample);
 };
-
-function demographicInfo(valueSelect) {
-    var filteredValue = data.metadata.filter(d => d.id == +valueSelect);
-
-    var panel = d3.select(".panel-body");
-    panel.html("");
-    panel.append("p").text(`ID: ${filteredValue[0].id}`);
-    panel.append("p").text(`Ethnicity: ${filteredValue[0].ethnicity}`);
-    panel.append("p").text(`Gender: ${filteredValue[0].gender}`);
-    panel.append("p").text(`Age: ${filteredValue[0].age}`);
-    panel.append("p").text(`Location: ${filteredValue[0].location}`);
-    panel.append("p").text(`Bbtype: ${filteredValue[0].bbtype}`);
-    panel.append("p").text(`Wfreq: ${filteredValue[0].wfreq}`);
-    }
-
-function barChart(valueSelect) {
-  var filteredValue = data.samples.filter(d => d.id == +valueSelect);
-  var ouid = filteredValue.map(d => d.otu_ids);
-  ouid = fixOuid(ouid[0].slice(0, 10));
-  var valueX = filteredValue.map(d => d.sample_values);
-  valueX = valueX[0].slice(0, 10);
-
-  var otu_label = filteredValue.map(d => d.otu_labels);
-  var names = fixBacteria(otu_label[0]).slice(0, 10);
-  console.log(ouid);
-  console.log(valueX);
-  console.log(otu_label);
-  console.log(names);
-
-  var trace = {
-    x: valueX,
-    y: ouid,
-    text: names,
-    type: "bar",
-    orientation: "h"
-  };
-
-  var layout = {
-    yaxis: {
-      autorange: "reversed"
-    }
-  };
-
-  var barData = [trace];
-
-  Plotly.newPlot("bar", barData, layout);
-}
-
-
-function bubbleChart(valueSelect) {
-  var filteredValue = data.samples.filter(value => value.id == +valueSelect);
-  var ouid = filteredValue.map(d => d.otu_ids);
-  ouid = ouid[0];
-  var yValue = filteredValue.map(d => d.sample_values);
-  yValue = yValue[0];
-
-  var otu_label = filteredValue.map(d => d.otu_labels);
-  otu_label = fixBacteria(otu_label[0]);
-
-  var trace1 = {
-    x: ouid,
-    y: yValue,
-    mode: "markers",
-    marker: {
-      color: ouid,
-      size: yValue
-    },
-    text: otu_label
-  };
-
-  var bubbleData = [trace1];
-
-  var layout = {
-    showlegend: false,
-    xaxis: { title: "OTU ID" }
-  };
-
-  Plotly.newPlot("bubble", bubbleData, layout);
-}
-
-init();
